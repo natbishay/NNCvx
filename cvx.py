aa22ecf627dd97c498d4c6d970602ba46778c586
natbishay <46910864+natbishay@users.noreply.github.com>
2022-05-05 20:45:09 -0700

    Hi guys, have fun

---
+++ cvx.py
@@ -0,0 +1,11 @@
+# The point of this is to implement an equivalent yet convex representation of the same neural net. 
+
+# Initial tests will be on this super cute data set
+import numpy as np
+
+n = 5
+X = np.array([-2, -1, 0, 1, 2])
+y = np.array([1, -1, 1, 1, -1])
+
+
+
---
+++ nn.py
@@ -0,0 +1,7 @@
+# The point of this is to implement a typical neural net with 1 hidden layer and ReLU activation. 
+
+import numpy as np
+
+n = 5
+X = np.array([-2, -1, 0, 1, 2])
+y = np.array([1, -1, 1, 1, -1])
