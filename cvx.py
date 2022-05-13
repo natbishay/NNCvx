a175908443611ae3748474a100570d8098d1fe36
natbishay <46910864+natbishay@users.noreply.github.com>
2022-05-13 15:23:22 -0700

    prints neurons that are nonzero

--- cvx.py
+++ cvx.py
@@ -4,54 +4,64 @@
 import numpy as np
 import cvxpy as cp
 from math import isclose
+import matplotlib.pyplot as plt
 
+np.random.seed(10)
 n = 10
 d = 3
 X = np.random.randn(n,d-1)
 X = np.append(X,np.ones((n,1)), axis=1)
 y=((np.linalg.norm(X[:,0:d-1],axis=1)>1)-0.5)*2
 
-beta = 1e-4
+# n=5
+# d=1
+# X = np.array([-2, -1, 0, 1, 2]).reshape(n,d)
+# y = np.array([1, -1, 1, 1, -1]).reshape(n,d)
 
-def relu(x):
-    return np.maximum(0,x)
+beta = 1e-4
+betas = [np.exp(j-10) for j in range(10)]
+
 def drelu(x):
     return x>=0
 
-D=np.empty((n,0))
+losses = []
 
-## Finite approximation of all possible sign patterns
-for i in range(int(1e2)):
-    u=np.random.randn(d,1)
-    D=np.append(D,drelu(np.dot(X,u)),axis=1)
+for beta in betas:
+    D=np.empty((n,0))
 
-D=(np.unique(D,axis=1))
-m = D.shape[1]
+    ## Finite approximation of all possible sign patterns
+    for i in range(int(1e2)):
+        u=np.random.randn(d,1)
+        D=np.append(D,drelu(np.dot(X,u)),axis=1)
 
-v = cp.Variable((d, m)) # 3 by 36
-w = cp.Variable((d, m))
+    D=(np.unique(D,axis=1))
+    m = D.shape[1]
 
-obj_a = 0
-obj_b = 0
+    v = cp.Variable((d, m)) # d by m
+    w = cp.Variable((d, m))
 
-constraints = []
+    constraints = []
+
+    #obj_a = cp.reshape(cp.sum(cp.multiply(D , (X@(v - w))), axis=1), (n,d))
+    obj_a = cp.sum(cp.multiply(D , (X@(v - w))), axis=1)
+    cost=cp.sum(cp.pos(1-cp.multiply(y,obj_a)))/n + beta*(cp.mixed_norm(v.T,2,1)+cp.mixed_norm(w.T,2,1))
 
 
-obj_a = cp.sum(cp.multiply(D , (X@(v - w))), axis=1)
-cost=cp.sum(cp.pos(1-cp.multiply(y,obj_a)))/n+beta*(cp.mixed_norm(v.T,2,1)+cp.mixed_norm(w.T,2,1))
+    constraints += [cp.multiply(2*D - np.ones((n,m)), (X@v)) >= 0]
+    constraints += [cp.multiply(2*D - np.ones((n,m)), (X@w)) >= 0]
 
+    obj_val = cp.Minimize(cost)
 
-constraints += [cp.multiply(2*D - np.ones((n,m)), (X@v)) >= 0]
-constraints += [cp.multiply(2*D - np.ones((n,m)), (X@w)) >= 0]
+    prob = cp.Problem(obj_val, constraints)
+    prob.solve()
+    # print("BETA =", beta)
+    # print(prob.value)
+    losses.append(prob.value)
+    # print(v.value)
+    # print(w.value)
 
-obj_val = cp.Minimize(cost)
-
-prob = cp.Problem(obj_val, constraints)
-prob.solve()
-
-print(prob.value)
-
-# uj is 1x3
+plt.semilogy(betas,losses)
+plt.savefig('betas.jpg')
 
 u = np.zeros((d,m))
 alpha = np.zeros((m))
@@ -66,5 +76,7 @@ for i in range(m):
             u[:,i] = w.value[:,i]/np.sqrt(norm_wi)
             alpha[i] = np.sqrt(norm_wi)
     else:
-         u[:,i] = v.value[:,i]/np.sqrt(norm_vi)
-         alpha[i] = np.sqrt(norm_vi)
+        u[:,i] = v.value[:,i]/np.sqrt(norm_vi)
+        alpha[i] = np.sqrt(norm_vi)
+print(alpha)
+print(np.nonzero(alpha))
